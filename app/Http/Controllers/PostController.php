a4cb009a44dfb8158ada9e0ac64bda256252c8a6
gidimotje <gidimotje.gvk@gmail.com>
2016-12-16 03:29:22 +0100

    Delete protection

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,6 +31,9 @@ class PostController extends Controller
 	public function getDeletePost($post_id)
 	{
 		$post = Post::where('id', $post_id)->first();
+		if (Auth::user() != $post->user) {
+			return redirect()->back();
+		}
 		$post->delete();
 		return redirect()->route('dashboard')->with(['message' => 'Successfully deleted']);
 	}
