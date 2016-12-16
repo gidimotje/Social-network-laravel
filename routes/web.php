65e380085ffc7f23c6171fefe41468eb87f168b9
gidimotje <gidimotje.gvk@gmail.com>
2016-12-16 04:30:46 +0100

    Edit posts functionality

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ Route::get('/delete-post/{post_id}', [
 	'middleware' => 'auth'
 ]);
 
-Route::post('/edit', function(\Illuminate\Http\Request $request){
-	return response()->json(['message' => $request['postId']]);
-})->name('edit');
+Route::post('/edit', [
+	'uses' => 'PostController@postEditPost',
+	'as' => 'edit'
+]);
